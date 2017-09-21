d9a32e9306c05c52088c1082845ca4b8cdbe9fcb
Cedric Legendre <contact@cackbone.fr>
2017-09-21 19:25:54 +0200

    First commit, bis

---
+++ target/debug/libRust_2048.d
@@ -0,0 +1,1 @@
+/home/orion/Documents/programming/Rust/Rust_2048/target/debug/libRust_2048.rlib: /home/orion/Documents/programming/Rust/Rust_2048/src/lib.rs
---
+++ target/debug/rust_2048.d
@@ -0,0 +1,1 @@
+/home/orion/Documents/programming/Rust/Rust_2048/target/debug/rust_2048: /home/orion/Documents/programming/Rust/Rust_2048/src/main.rs
